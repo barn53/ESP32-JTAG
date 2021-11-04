d882134dc705569f428228d2d48ef87ba97a89bc
Gerd Müller <gerdmuller.de@gmail.com>
2021-11-04 22:49:53 +0100

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    pinMode(2, OUTPUT);
+    Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+uint32_t c { 0 };
+void loop()
+{
+    static uint32_t s;
+    s = c;
+    uint32_t l { c };
+    digitalWrite(2, !digitalRead(2));
+    Serial.printf("count: %u - %u - %u\n", c, s, l);
+    ++c;
+    delay(200);
 }
